f5f81e856eed127b369472aaec98762e8f11bd8c
Chris <chriswang001121@gmail.com>
2025-03-24 09:56:52 +0800

    Create main.R
    Added project and author information in main.R

---
+++ main.R
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# scFlowKit: A Modular Single-Cell RNA-seq Analysis Pipeline
+#-------------------------------------------------------------------------------
+
+# 项目信息
+# Name: scFlowKit
+# Description: 一个模块化的单细胞 RNA-seq 分析流程，支持 R 和 Python
+# GitHub: https://github.com/chriswang001121/scFlowKit
+# License: MIT License
+# Version: [占位，待后续补充]
+
+# 作者信息
+# Name: chriswang001121
+# Email: chriswang001121@gmail.com
+# Homepage: https://scholar.pulppoetry.org
+# LinkedIn: https://www.linkedin.com/in/yibin-zhou
+
+
+# 当前版本功能
+# - 数据加载：支持 10X Genomics 数据格式（.h5 或 .mtx）
+
+#-------------------------------------------------------------------------------
+
